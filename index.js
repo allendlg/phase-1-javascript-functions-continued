e8282883350498e6c0aa383be1f7dbe22b737f1c
allendlg <allen.delagarza01@gmail.com>
2021-06-29 10:11:45 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+// Your code here   
+function saturdayFun(activity = `roller-skate`){
+        return (`This Saturday, I want to ${activity}!`);
+
+}
+
+function mondayWork(activity = `go to the office`){
+    return (`This Monday, I will ${activity}.`);
+
+}
+
+function wrapAdjective(name = 'a hard worker') {
+    const test123 = `You are`
+        return function (){
+            return `${test123} *${name}*!`;
+    
+            
+        }
+   }
+  
+
+   
+   
+//function razzle(lawyer="Billy", target="'em") {
+ //   console.log(`${lawyer} razzle-dazzles ${target}!`);
+//}
+//razzle(); //=> Billy razzle-dazzles 'em!
+//razzle("Methuselah", "T'challah"); //=> Methuselah razzle-dazzles T'challah! //
